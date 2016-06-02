f6b8b28786cd083f1e1799ea27a7e3b5169f6fd5
Dahoe Shin <kity990304@gmail.com>
2016-06-02 20:59:03 +0900

    #2 Event

--- app/src/main/java/kr/hs/emirim/dahoe0304/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dahoe0304/simplewebbrowser/MainActivity.java
@@ -3,7 +3,9 @@ package kr.hs.emirim.dahoe0304.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,7 +13,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText editUrl;
     Button butMove, butPrev;
     WebView web;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -20,8 +21,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butPrev=(Button)findViewById(R.id.but_prev);
         web=(WebView)findViewById(R.id.web);
         butMove.setOnClickListener(this);
-    }
+        butPrev.setOnClickListener(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webSet=web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+        web.loadUrl("http://www.naver.com");
 
+    }
     /**
      * Called when a view has been clicked.
      *
@@ -29,7 +36,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {   //이벤트 헨들러까지 겸직! 이랑 다른것도 한개더..
+        String url=null;
+        switch (v.getId()){
+            case R.id.but_move:
+                url=editUrl.getText().toString();
+                if(!url.contains("http"))
+                    url="http://"+url;
+                web.loadUrl(url);
+                break;
+            case R.id.but_prev:
+                web.goBack();
+                break;
 
+        }
 
     }
 }
